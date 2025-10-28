13212f56704638f5d734f19ce1541df00ebd4f82
Pooya Parsa <pooya@pi0.io>
2025-10-28 13:41:44 +0100

    update

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ const app = new Hono()
 const runtime = process.versions.bun ? `Bun@${process.versions.bun}` : `Node@${process.versions.node}`
 
 app.get("/", (c) => {
-  return c.text(`Hello Nitro with Hono! (Runtime: ${runtime})`)
+  return c.text(`Nitro + Hono Works! (Runtime: ${runtime})`)
 })
 
 export default app
